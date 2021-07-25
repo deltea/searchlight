f73c624d03dd5543f1fbb5aa8637d34ff1fa199e
Leo <thcheetah777@gmail.com>
2021-07-25 10:56:36 -0700

    Add code to script.js

---
+++ js/script.js
@@ -0,0 +1,41 @@
+// Searchlight
+// Game objects
+let game = {};
+let stats = {};
+let sfx = {};
+
+// Phaser configuration
+const config = {
+  type: Phaser.AUTO,
+
+  // Size of game window
+  scale: {
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    mode: Phaser.Scale.FIT,
+  },
+
+  // Arcade physics
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: {
+        y: 1500
+      },
+
+      // Options
+      enableBody: true,
+      // NOTE: Uncomment the following line to enable debug mode
+      // debug: true
+    }
+  },
+
+  // Scenes
+  scene: {
+    preload,
+    create,
+    update
+  }
+};
+
+// Phaser game
+const phaserGame = new Phaser.Game(config);
