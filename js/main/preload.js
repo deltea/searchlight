10f2ee14e574c8d31f6ae5cecc6919eb1dad7fa5
Leo <thcheetah777@gmail.com>
2021-07-25 17:33:17 -0700

    Load spritesheets

--- js/main/preload.js
+++ js/main/preload.js
@@ -1,3 +1,9 @@
 // Load media here
 preload() {
+  // Spritesheets
+  this.load.spritesheet("characters", "assets/imgs/characters.png", {
+    frameWidth: 16,
+    frameHeight: 16,
+    spacing: 1
+  });
 }
