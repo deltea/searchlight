be9385dc59fd48745b58e2e640d8549e81ca4cfb
Leo <thcheetah777@gmail.com>
2021-07-25 17:33:52 -0700

    Load anpther spritesheet

--- js/main/preload.js
+++ js/main/preload.js
@@ -6,4 +6,9 @@ preload() {
     frameHeight: 16,
     spacing: 1
   });
+  this.load.spritesheet("terrain", "assets/imgs/terrain.png", {
+    frameWidth: 16,
+    frameHeight: 16,
+    spacing: 1
+  });
 }
