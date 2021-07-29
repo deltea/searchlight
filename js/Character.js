78bf305e9617ef6719f32eca39f91063946950f4
Leo <thcheetah777@gmail.com>
2021-07-29 16:58:50 -0700

    Add the clothes and sprite to the Character class

--- js/Character.js
+++ js/Character.js
@@ -1,22 +1,28 @@
 // Create characters using this class
 class Character {
   // Initialize character
-  constructor(x, y, clothesId, type) {
+  constructor(x, y, clothesId, spritesheet, ype, group, phaser) {
     // Position of character
     this.x = x;
     this.y = y;
 
     // Types: Player, Target, Normal
-    this.type = type
+    this.type = type;
 
     // An array of where the clothes are in the spritesheet
     this.clothesId = clothesId;
 
+    // The spritesheet to look in
+    this.spritesheet = spritesheet;
+
+    // Group of phaser sprites to add to
+    this.group = group;
+
+    // HACK: Pass in phaser
+    this.phaser = phaser;
+
     // Check which type of character the character is
     switch (this.type) {
-      case "player":
-        this.target = false;
-        break;
       case "target":
         this.target = true;
         break;
@@ -24,6 +30,38 @@ class Character {
         this.target = false;
         break;
     }
+
+    // Main code for character
+    // Create character sprite with no Y-axis gravity
+    this.sprite = this.group.create(x, y, this.spritesheet).setGravityY(-config.physics.arcade.gravity.y);
+
+    // The skin color
+    this.sprite.setFrame(this.clothesId["skin"]);
+
+    // Create the clothes
+    // Pants
+    this.sprite.pants = this.phaser.add.image(x, y, this.spritesheet);
+    this.sprite.setFrame(this.clothesId["pants"]);
+
+    // Shirt
+    this.sprite.shirt = this.phaser.add.image(x, y, this.spritesheet);
+    this.sprite.setFrame(this.clothesId["shirt"]);
+
+    // Hair
+    this.sprite.hair = this.phaser.add.image(x, y, this.spritesheet);
+    this.sprite.setFrame(this.clothesId["hair"]);
+
+    // Hat
+    this.sprite.hat = this.phaser.add.image(x, y, this.spritesheet);
+    this.sprite.setFrame(this.clothesId["hat"]);
+
+    // Shield
+    this.sprite.shield = this.phaser.add.image(x, y, this.spritesheet);
+    this.sprite.setFrame(this.clothesId["shield"]);
+
+    // Weapon
+    this.sprite.weapon = this.phaser.add.image(x, y, this.spritesheet);
+    this.sprite.setFrame(this.clothesId["weapon"]);
   }
 
   // Update characters, runs in the main update function
