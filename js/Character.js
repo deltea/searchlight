68cae56dac70d71c6155c2fe431ca953c4b54b02
Leo <thcheetah777@gmail.com>
2021-07-29 17:23:10 -0700

    Test Character class and fix jshint issue

--- js/Character.js
+++ js/Character.js
@@ -1,7 +1,7 @@
 // Create characters using this class
 class Character {
   // Initialize character
-  constructor(x, y, clothesId, spritesheet, ype, group, phaser) {
+  constructor(x, y, clothesId, spritesheet, type, group, phaser) {
     // Position of character
     this.x = x;
     this.y = y;
@@ -36,32 +36,33 @@ class Character {
     this.sprite = this.group.create(x, y, this.spritesheet).setGravityY(-config.physics.arcade.gravity.y);
 
     // The skin color
-    this.sprite.setFrame(this.clothesId["skin"]);
+    this.sprite.setFrame(this.clothesId.skin);
 
     // Create the clothes
     // Pants
     this.sprite.pants = this.phaser.add.image(x, y, this.spritesheet);
-    this.sprite.setFrame(this.clothesId["pants"]);
+    this.sprite.setFrame(this.clothesId.pants);
 
     // Shirt
     this.sprite.shirt = this.phaser.add.image(x, y, this.spritesheet);
-    this.sprite.setFrame(this.clothesId["shirt"]);
+    this.sprite.setFrame(this.clothesId.shirt);
 
     // Hair
     this.sprite.hair = this.phaser.add.image(x, y, this.spritesheet);
-    this.sprite.setFrame(this.clothesId["hair"]);
+    this.sprite.setFrame(this.clothesId.hair);
 
     // Hat
     this.sprite.hat = this.phaser.add.image(x, y, this.spritesheet);
-    this.sprite.setFrame(this.clothesId["hat"]);
+    this.sprite.setFrame(this.clothesId.hat);
 
     // Shield
     this.sprite.shield = this.phaser.add.image(x, y, this.spritesheet);
-    this.sprite.setFrame(this.clothesId["shield"]);
+
+    this.sprite.setFrame(this.clothesId.shield);
 
     // Weapon
     this.sprite.weapon = this.phaser.add.image(x, y, this.spritesheet);
-    this.sprite.setFrame(this.clothesId["weapon"]);
+    this.sprite.setFrame(this.clothesId.weapon);
   }
 
   // Update characters, runs in the main update function
--- js/main/create.js
+++ js/main/create.js
@@ -1,4 +1,13 @@
 // Create sprites
 function create() {
-
+  // Testing Character class
+  game.characters = this.physics.add.group();
+  new Character(200, 200, {
+    skin: 3,
+    shirt: 6,
+    hair: 20,
+    hat: 29,
+    shield: 39,
+    weapon: 52
+  }, "characters", "normal", game.characters, this);
 }
